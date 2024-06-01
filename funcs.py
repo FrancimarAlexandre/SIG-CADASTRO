2db3e278f918f53218da3dc4e0b2a62945f32a6a
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-06-01 17:26:40 -0300

    update main.py/funcs.py: exibindo os cadastro

--- funcs.py
+++ funcs.py
@@ -1,12 +1,10 @@
 import sqlite3
 
 class DataBase:
-    # criando e conectando banco de dados
     def connect_bd(self):
         self.conn = sqlite3.connect("DATABASE.db")
         self.cursor = self.conn.cursor()
         
-        # Criando a tabela
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS Cadastro (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,26 +18,28 @@ class DataBase:
                 estado VARCHAR(255) NOT NULL,
                 numero INT NOT NULL
             )
-            """)
+        """)
         self.conn.commit()
+    
     def desconnect_bd(self):
         self.cursor.close()
-    # CREATE
-    def insert_dados(self,nome,email,idade,telefone,rua,bairro,cidade,estado,numero):
+    
+    def insert_dados(self, nome, email, idade, telefone, rua, bairro, cidade, estado, numero):
         self.connect_bd()
         self.cursor.execute("""
-            INSERT INTO Cadastro (nome, email, idade,telefone, rua, bairro, cidade, estado, numero)
+            INSERT INTO Cadastro (nome, email, idade, telefone, rua, bairro, cidade, estado, numero)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
         """, (nome, email, idade, telefone, rua, bairro, cidade, estado, numero))
         
         self.conn.commit()
         self.desconnect_bd()
-    # SELECT
+    
     def select_dados(self):
         self.connect_bd()
         self.cursor.execute("SELECT * FROM Cadastro")
         dados = self.cursor.fetchall()
         self.desconnect_bd()
         return dados
-a= DataBase()
+
+a = DataBase()
 a.connect_bd()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import customtkinter as ctk# importando a biblioteca
+import customtkinter as ctk
 from funcs import DataBase
 import tkinter as tk
 from tkinter import ttk
-# class App Principal
+
 class App():
     def __init__(self):
         self.janela = janela
@@ -13,67 +13,68 @@ class App():
     
     def TabViews(self):
         # criando e posicionando as telas das abas
-        self.tabview = ctk.CTkTabview(self.janela,width=1230,height=710,bg_color="#444444")
-        self.tabview.place(x=10,y=35)
+        self.tabview = ctk.CTkTabview(self.janela, width=1230, height=710, bg_color="#444444")
+        self.tabview.place(x=10, y=35)
         # criando as títulos das abas
         self.inicio_tab = self.tabview.add("Cadastro")
-        self.cadastroView_tab = self.tabview.add("exibir cadastros")
-        self.credito_tab = self.tabview.add("créditos")
-
+        self.cadastroView_tab = self.tabview.add("Exibir Cadastros")
+        self.credito_tab = self.tabview.add("Créditos")
 
     def TelaInicial(self):
         # Dados Pessoais
-        self.LabelInfoDadosPessoais = ctk.CTkLabel(self.inicio_tab,text="DADOS PESSOAIS",font=("moderna",20)).place(relx=0.35,rely=0.01)
+        self.LabelInfoDadosPessoais = ctk.CTkLabel(self.inicio_tab, text="DADOS PESSOAIS", font=("moderna", 20)).place(relx=0.35, rely=0.01)
         # Nome usuário
-        self.LabelNome = ctk.CTkLabel(self.inicio_tab,text="Nome",font=("moderna",20)).place(relx=0.01,rely=0.02)
-        self.EntryNome = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryNome.place(relx=0.01,rely=0.06,relwidth=0.55)
+        self.LabelNome = ctk.CTkLabel(self.inicio_tab, text="Nome", font=("moderna", 20)).place(relx=0.01, rely=0.02)
+        self.EntryNome = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryNome.place(relx=0.01, rely=0.06, relwidth=0.55)
         # Idade usuário
-        self.LabelIdade = ctk.CTkLabel(self.inicio_tab,text="Idade",font=("moderna",20)).place(relx=0.58,rely=0.02)
-        self.EntryIdade = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryIdade.place(relx=0.58,rely=0.06,relwidth=0.05)
+        self.LabelIdade = ctk.CTkLabel(self.inicio_tab, text="Idade", font=("moderna", 20)).place(relx=0.58, rely=0.02)
+        self.EntryIdade = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryIdade.place(relx=0.58, rely=0.06, relwidth=0.05)
         # Email usuário
-        self.LabelGmail = ctk.CTkLabel(self.inicio_tab,text="Gmail",font=("moderna",20)).place(relx=0.01,rely=0.11)
-        self.EntryGmail = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryGmail.place(relx=0.01,rely=0.16,relwidth=0.55)
+        self.LabelGmail = ctk.CTkLabel(self.inicio_tab, text="Gmail", font=("moderna", 20)).place(relx=0.01, rely=0.11)
+        self.EntryGmail = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryGmail.place(relx=0.01, rely=0.16, relwidth=0.55)
         # Telefone usuário
-        self.LabelTelefone = ctk.CTkLabel(self.inicio_tab,text="Telefone",font=("moderna",20)).place(relx=0.58,rely=0.11)
-        self.EntryTelefone = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryTelefone.place(relx=0.58,rely=0.16,relwidth=0.20)
+        self.LabelTelefone = ctk.CTkLabel(self.inicio_tab, text="Telefone", font=("moderna", 20)).place(relx=0.58, rely=0.11)
+        self.EntryTelefone = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryTelefone.place(relx=0.58, rely=0.16, relwidth=0.20)
         
         # Endereço
-        self.LabelInfoDadosPessoais = ctk.CTkLabel(self.inicio_tab,text="ENDEREÇO",font=("moderna",20)).place(relx=0.35,rely=0.23)
+        self.LabelInfoDadosPessoais = ctk.CTkLabel(self.inicio_tab, text="ENDEREÇO", font=("moderna", 20)).place(relx=0.35, rely=0.23)
         # RUA
-        self.LabelRua = ctk.CTkLabel(self.inicio_tab,text="Rua",font=("moderna",20)).place(relx=0.01,rely=0.24)
-        self.EntryRua = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryRua.place(relx=0.01,rely=0.29,relwidth=0.4)
+        self.LabelRua = ctk.CTkLabel(self.inicio_tab, text="Rua", font=("moderna", 20)).place(relx=0.01, rely=0.24)
+        self.EntryRua = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryRua.place(relx=0.01, rely=0.29, relwidth=0.4)
         # BAIRRO
-        self.LabelBairro = ctk.CTkLabel(self.inicio_tab,text="Bairro",font=("moderna",20)).place(relx=0.5,rely=0.24)
-        self.EntryBairro = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryBairro.place(relx=0.5,rely=0.29,relwidth=0.4)
+        self.LabelBairro = ctk.CTkLabel(self.inicio_tab, text="Bairro", font=("moderna", 20)).place(relx=0.5, rely=0.24)
+        self.EntryBairro = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryBairro.place(relx=0.5, rely=0.29, relwidth=0.4)
         # CIDADE
-        self.LabelCidade = ctk.CTkLabel(self.inicio_tab,text="Cidade",font=("moderna",20)).place(relx=0.01,rely=0.34)
-        self.EntryCidade = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryCidade.place(relx=0.01,rely=0.39,relwidth=0.4)
+        self.LabelCidade = ctk.CTkLabel(self.inicio_tab, text="Cidade", font=("moderna", 20)).place(relx=0.01, rely=0.34)
+        self.EntryCidade = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryCidade.place(relx=0.01, rely=0.39, relwidth=0.4)
         # ESTADO
-        self.LabelEstado = ctk.CTkLabel(self.inicio_tab,text="Estado",font=("moderna",20)).place(relx=0.5,rely=0.34)
-        self.EntryEstado = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryEstado.place(relx=0.5,rely=0.39,relwidth=0.4)
+        self.LabelEstado = ctk.CTkLabel(self.inicio_tab, text="Estado", font=("moderna", 20)).place(relx=0.5, rely=0.34)
+        self.EntryEstado = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryEstado.place(relx=0.5, rely=0.39, relwidth=0.4)
         # NÙMERO
-        self.LabelNumero = ctk.CTkLabel(self.inicio_tab,text="N°",font=("moderna",20)).place(relx=0.01,rely=0.44)
-        self.EntryNumero = ctk.CTkEntry(self.inicio_tab,font=("mordena",20))
-        self.EntryNumero.place(relx=0.01,rely=0.49,relwidth=0.05)
-        self.ButtonCadastro = ctk.CTkButton(self.inicio_tab,text="CADASTRAR",font=("moderna",20),command=self.convert_dados).place(relx = 0.45,rely=0.55,relwidth=0.1)
+        self.LabelNumero = ctk.CTkLabel(self.inicio_tab, text="N°", font=("moderna", 20)).place(relx=0.01, rely=0.44)
+        self.EntryNumero = ctk.CTkEntry(self.inicio_tab, font=("moderna", 20))
+        self.EntryNumero.place(relx=0.01, rely=0.49, relwidth=0.05)
+        self.ButtonCadastro = ctk.CTkButton(self.inicio_tab, text="CADASTRAR", font=("moderna", 20), command=self.convert_dados).place(relx=0.45, rely=0.55, relwidth=0.1)
+    
     def limpar_tela(self):
-        self.EntryNome.delete(0,ctk.END)
-        self.EntryGmail.delete(0,ctk.END)
-        self.EntryIdade.delete(0,ctk.END)
-        self.EntryTelefone.delete(0,ctk.END)
-        self.EntryRua.delete(0,ctk.END)
-        self.EntryBairro.delete(0,ctk.END)
-        self.EntryCidade.delete(0,ctk.END)
-        self.EntryEstado.delete(0,ctk.END)
-        self.EntryNumero.delete(0,ctk.END)
+        self.EntryNome.delete(0, ctk.END)
+        self.EntryGmail.delete(0, ctk.END)
+        self.EntryIdade.delete(0, ctk.END)
+        self.EntryTelefone.delete(0, ctk.END)
+        self.EntryRua.delete(0, ctk.END)
+        self.EntryBairro.delete(0, ctk.END)
+        self.EntryCidade.delete(0, ctk.END)
+        self.EntryEstado.delete(0, ctk.END)
+        self.EntryNumero.delete(0, ctk.END)
+    
     def convert_dados(self):
         self.nome = self.EntryNome.get()
         self.email = self.EntryGmail.get()
@@ -86,10 +87,12 @@ class App():
         self.numero = self.EntryNumero.get()
 
         self.limpar_tela()
-        a = DataBase()
-        return a.insert_dados(self.nome,self.email,self.idade,self.telefone,self.rua,self.bairro,self.cidade,self.estado,self.numero)
+        db = DataBase()
+        db.insert_dados(self.nome, self.email, self.idade, self.telefone, self.rua, self.bairro, self.cidade, self.estado, self.numero)
+        self.update_treeview()
+    
     def cadastro_view(self):
-           # Crie o Treeview dentro do frame
+        # Crie o Treeview dentro do frame
         self.treeview = ttk.Treeview(self.cadastroView_tab, columns=("col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9", "col10"), show='headings')
         self.treeview.place(relx=0.01, rely=0.01, relwidth=0.97, relheight=0.95)
 
@@ -113,24 +116,24 @@ class App():
 
         self.treeview.configure(yscrollcommand=self.scrollbarY.set, xscrollcommand=self.scrollbarX.set)
 
-        # Preencher o Treeview com dados do banco de dados
-   
-        self.preencher_treeview()
+        self.update_treeview()
 
-    def preencher_treeview(self):
+    def update_treeview(self):
         db = DataBase()
         dados = db.select_dados()
         
-        for row in dados:
-            self.treeview.insert("", "end", values=row)
+        # Limpar o conteúdo atual do Treeview
+        for item in self.treeview.get_children():
+            self.treeview.delete(item)
+        
+        # Inserir os dados atualizados no Treeview
+        for i in dados:
+            self.treeview.insert("", ctk.END, values=i)
+           
 if "__main__":
     janela = ctk.CTk() # criando janela
-
     janela._set_appearance_mode('dark') # definindo tema da tela
-
     janela.geometry('1250x750') # definindo tamanho da tela 
-
     janela.title("SIG - CADASTRO") # definindo titilo na aplicação
-
-    janela.resizable(width=False,height=False) # definindo que a tela vai ter o tamanho fixo
+    janela.resizable(width=False, height=False) # definindo que a tela vai ter o tamanho fixo
     App()
